4285e04d5ad65715ce317f7c0d5f5f15daba3ed7
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-09-06 01:48:26 +0600

    project added

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Validator;
+
+class StudentController extends Controller
+{
+    public function index() {
+        $students = Student::all();
+
+        return response()->json([
+            'status' => 'true',
+            'data' => $students
+        ]);
+    }
+
+    public function store(Request $request) {
+        try{
+            $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|min:3',
+                'email' => 'required|email',
+                'phone' => 'nullable|min:10',
+            ]
+        );
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation failed.',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $student = new Student;
+
+        $student->name = $request->name;
+        $student->email = $request->email;
+        $student->phone = $request->phone;
+        $student->address = $request->address;
+
+        $student->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Student added successfully',
+            'data' => $student
+        ]);
+
+        } catch(\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation failed.',
+                'errors' => 'Exceptions: '.$e
+            ]);
+        }
+    }
+}
